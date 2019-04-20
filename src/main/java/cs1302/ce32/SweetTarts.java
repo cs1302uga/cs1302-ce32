66b0f0664f3bddbb518fd9a395de71eafa67fd14
bjb211 <bjb211@uga.edu>
2019-04-20 13:48:47 -0400

    cleaned up starter code for SweetTarts

--- src/main/java/cs1302/ce32/SweetTarts.java
+++ src/main/java/cs1302/ce32/SweetTarts.java
@@ -5,10 +5,25 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.InputStream;
 
+/**
+ * A sweet program that calculates the maximum overall enjoyment
+ * for eating rolls of Sweet Tarts given a preference for each color
+ * and a roll of candies. Input data is read from an input file. There
+ * can be multiple test cases per file. Each test begins with the number
+ * of candies in the roll. The next line of the file contains a {@code String}
+ * representing the preferences for each color. The first character in
+ * the preferences {@code String} is the most preferred candy (character)
+ * and the last character is the least preferred. The final line of the
+ * test case is the candy roll itself which is represented as a {@code String}
+ * where each character represents a candy.
+ */
 public class SweetTarts {
     
     private static final String INPUT_FILE = "sample2.txt";
-                                                     
+
+    /**
+     * The entry point for the application.
+     */
     public static void main(String[] args) {
         char[] pref = new char[4];
         int numCandies;
@@ -38,12 +53,14 @@ public class SweetTarts {
     /**
      * Returns the maximum value of eating a roll of sweet tarts (values)
      * starting at position {@code lo} and ending at position {@code hi}.
-     * The consumer is only allowed to eat from the ends of the roll.
+     * The consumer is only allowed to eat from the ends of the roll. The value
+     * for eating a particular candy is the value of that candy multiplied
+     * the turn in which it is eaten.
      *
      * @param values the values (enjoyment) of eating a particular candy.
-     * @param lo the starting index in the array
-     * @param hi the ending index in the array
-     * @param turn the current turn
+     * @param lo the starting index in the array.
+     * @param hi the ending index in the array.
+     * @param turn the current turn.
      */
     private static int max(int[] values, int lo, int hi, int turn) {
         // TODO: Implement me!
@@ -52,8 +69,8 @@ public class SweetTarts {
             
     /**
      * Print the current state of the game. Output includes
-     * the number of candies in the current iteration, the
-     * preferences and the values of each candy.
+     * the number of candies in the current iteration and the
+     * numeric enjoyment value of each candy.
      *
      * @param numCandies the number of candies.
      * @param values the values of the candies.
@@ -69,7 +86,7 @@ public class SweetTarts {
 
     /**
      * Reads the candy letters and turns them into their
-     * corresponding values using the pref array.
+     * corresponding values using the specified preferences for each.
      *
      * @param numCandies the number of candy values to read.
      * @param values the array in which to store the values.
@@ -86,8 +103,9 @@ public class SweetTarts {
     } // readValues
 
     /**
-     * Converts a character (candy) to its value based
-     * on the preferences.
+     * Converts a character (candy) to its value based on the preferences.
+     * The most preferred candy will have a value of 4 and the least preferred
+     * candy will have a value of 1. 
      *
      * @param candy the character (candy) value to convert.
      * @param pref the array of candy preferences.
@@ -117,8 +135,8 @@ public class SweetTarts {
 
     /**
      * Returns a scanner for a file with the specified filename.
-     * @param filename the file containing the names
-     * @return a scanner for the file
+     * @param filename the file containing the names.
+     * @return a scanner for the file.
      */
     public static Scanner loadFileScanner(String filename) throws FileNotFoundException {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
